7477bd173f062886a03529fbedb9c56fdafb408e
Stepan <srekhlyasov@gmail.com>
2026-06-18 08:50:48 +0400

    dockerfile-hack

--- backend/Program.cs
+++ backend/Program.cs
@@ -100,4 +100,9 @@ app.UseAuthentication();
 app.UseMiddleware<LastActivityMiddleware>();
 app.UseAuthorization();
 app.MapControllers();
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+app.MapFallbackToFile("index.html");
+
 app.Run();
